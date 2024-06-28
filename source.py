7dc6db73565ffa61a68111cc7e9e16791de55847
IlhamApriansyah <ilhamapriansyah471@gmail.com>
2024-06-28 09:35:59 +0700

    update

--- source.py
+++ source.py
@@ -1,18 +1,16 @@
 import os
 import telebot
+import logging
 from telebot import types
 import random as rd
+
+logging.basicConfig(level=logging.INFO)
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
 game = ['Mobile Legends', 'Free Fire', 'Genshin Impact', 'Final Fantasy', 'Harvest Moon']
 
-@bot.message_handler(commands=['reset'])
-def send_message(message):
-    markup = types.ReplyKeyboardRemove(selective=False)
-    bot.send_message(chat_id=message.chat.id)
-    
 @bot.message_handler(commands=['mulai'])
 def send_welcome(message):
     bot.reply_to(message, "Masukan nama anda :")
@@ -22,6 +20,9 @@ def echo_all(message):
     bot.reply_to(message, "Hai, " + message.text)
     bot.reply_to(message, "Pasti game kesukaan kamu : " + rd.choice(game))
 
+@bot.message_handler(commands=['reset'])
+def handle_reset_command(message):
+    bot.reply_to(message, "Reset berhasil!")
 
 bot.infinity_polling()
 
